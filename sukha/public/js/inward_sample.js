f1361f102c005a27da0ace156d859a0caa014c64
Finbyz <info@finbyz.tech>
2026-06-03 09:30:09 +0000

    Update inward with changes and cost sheet for EWX

---
+++ sukha/public/js/inward_sample.js
@@ -0,0 +1,297 @@
+// frappe.ui.form.on("Inward Sample", {
+//     refresh(frm) {
+
+//         if (!frm.doc.currency) {
+//             frm.set_value("currency", frappe.boot.sysdefaults.currency);
+//         }
+
+//         calculate_total_evaluation_cost(frm);
+//     },
+
+//     custom_sample_cost(frm) {
+//         calculate_total_evaluation_cost(frm);
+//     },
+
+//     custom_courier_cost(frm) {
+//         calculate_total_evaluation_cost(frm);
+//     },
+
+//     custom_testing_cost(frm) {
+//         calculate_total_evaluation_cost(frm);
+//     },
+
+//     async item_code(frm) {
+
+//         if (!frm.doc.item_code) {
+//             frm.set_value("custom_un_no", "");
+//             frm.set_value("custom_batch_no", "");
+//             frm.set_value("custom_manufacturing_date", "");
+//             return;
+//         }
+
+//         // Fetch UN No from Item Compliance child table
+//         let item = await frappe.db.get_doc("Item", frm.doc.item_code);
+
+//         if (item.custom_item_compliance_and_export?.length) {
+//             frm.set_value(
+//                 "custom_un_no",
+//                 item.custom_item_compliance_and_export[0].un_number || ""
+//             );
+//         }
+
+//         // Fetch latest batch for selected item
+//         frappe.call({
+//             method: "frappe.client.get_list",
+//             args: {
+//                 doctype: "Batch",
+//                 filters: {
+//                     item: frm.doc.item_code,
+//                     disabled: 0
+//                 },
+//                 fields: [
+//                     "name",
+//                     "manufacturing_date",
+//                     "creation"
+//                 ],
+//                 order_by: "creation desc",
+//                 limit_page_length: 1
+//             },
+//             callback: function(r) {
+
+//                 if (r.message && r.message.length) {
+
+//                     let batch = r.message[0];
+
+//                     frm.set_value(
+//                         "custom_batch_no",
+//                         batch.name
+//                     );
+
+//                     frm.set_value(
+//                         "custom_manufacturing_date",
+//                         batch.manufacturing_date
+//                     );
+//                 }
+//             }
+//         });
+//     },
+
+//     custom_batch_no(frm) {
+
+//         if (!frm.doc.custom_batch_no) return;
+
+//         frappe.db.get_value(
+//             "Batch",
+//             frm.doc.custom_batch_no,
+//             "manufacturing_date"
+//         ).then(r => {
+
+//             if (r.message) {
+//                 frm.set_value(
+//                     "custom_manufacturing_date",
+//                     r.message.manufacturing_date
+//                 );
+//             }
+//         });
+//     }
+// });
+
+// function calculate_total_evaluation_cost(frm) {
+
+//     const total =
+//         flt(frm.doc.custom_sample_cost) +
+//         flt(frm.doc.custom_courier_cost) +
+//         flt(frm.doc.custom_testing_cost);
+
+//     frm.set_value(
+//         "custom_total_evaluation_cost",
+//         total
+//     );
+// }
+
+
+
+frappe.ui.form.on("Inward Sample", {
+    setup(frm) {
+        frm.set_query("custom_batch_no_", function () {
+            return {
+                filters: {
+                    item: frm.doc.item_code,
+                    disabled: 0
+                }
+            };
+        });
+    },
+
+    refresh(frm) {
+
+        if (!frm.doc.currency) {
+            frm.set_value("currency", frappe.boot.sysdefaults.currency);
+        }
+
+        calculate_total_evaluation_cost(frm);
+    },
+
+    custom_sample_cost(frm) {
+        calculate_total_evaluation_cost(frm);
+    },
+
+    custom_courier_cost(frm) {
+        calculate_total_evaluation_cost(frm);
+    },
+
+    custom_testing_cost(frm) {
+        calculate_total_evaluation_cost(frm);
+    },
+
+    async item_code(frm) {
+
+        frm.set_value("custom_un_no", "");
+        frm.set_value("custom_batch_no_", "");
+        frm.set_value("custom_manufacturing_date", "");
+
+        if (!frm.doc.item_code) return;
+
+        // Fetch UN No from Item Compliance child table
+        let item = await frappe.db.get_doc("Item", frm.doc.item_code);
+
+        if (item.custom_item_compliance_and_export?.length) {
+            frm.set_value(
+                "custom_un_no",
+                item.custom_item_compliance_and_export[0].un_number || ""
+            );
+        }
+    },
+
+    custom_batch_no(frm) {
+
+        if (!frm.doc.custom_batch_no) {
+            frm.set_value("custom_manufacturing_date", "");
+            return;
+        }
+
+        // Fetch Manufacturing Date from selected Batch
+        frappe.db.get_value(
+            "Batch",
+            frm.doc.custom_batch_no,
+            "manufacturing_date"
+        ).then(r => {
+
+            if (r.message) {
+                frm.set_value(
+                    "custom_manufacturing_date",
+                    r.message.manufacturing_date
+                );
+            }
+        });
+    }
+});
+
+function calculate_total_evaluation_cost(frm) {
+
+    const total =
+        flt(frm.doc.custom_sample_cost) +
+        flt(frm.doc.custom_courier_cost) +
+        flt(frm.doc.custom_testing_cost);
+
+    frm.set_value(
+        "custom_total_evaluation_cost",
+        total
+    );
+}
+
+frappe.ui.form.on("Inward Sample", {
+
+    async party(frm) {
+
+        if (frm.doc.link_to !== "Supplier" || !frm.doc.party) {
+            return;
+        }
+
+        try {
+
+            // Supplier Details
+            let supplier = await frappe.db.get_doc(
+                "Supplier",
+                frm.doc.party
+            );
+
+            frm.set_value(
+                "custom_supplier_name_",
+                supplier.supplier_name || ""
+            );
+
+            frm.set_value(
+                "custom_supplier_type",
+                supplier.supplier_type || ""
+            );
+
+            // Primary Contact
+            if (supplier.supplier_primary_contact) {
+
+                let contact = await frappe.db.get_doc(
+                    "Contact",
+                    supplier.supplier_primary_contact
+                );
+
+                frm.set_value(
+                    "custom_contact_person",
+                    contact.first_name || contact.full_name || ""
+                );
+
+                frm.set_value(
+                    "custom_email_id",
+                    contact.email_id || ""
+                );
+
+                console.log("Contact:", contact);
+                console.log("phone:", contact.phone);
+                console.log("mobile_no:", contact.mobile_no);
+                console.log("phone_nos:", contact.phone_nos);
+
+                let mobile =
+                    contact.phone ||
+                    contact.mobile_no ||
+                    (contact.phone_nos?.length
+                        ? contact.phone_nos[0].phone
+                        : "");
+
+                console.log("Final Mobile:", mobile);
+
+                await frm.set_value(
+                    "custom_mobilewhatsapp",
+                    mobile
+                );
+
+                frm.refresh_field("custom_mobilewhatsapp");
+            }
+
+            // Primary Address
+            if (supplier.supplier_primary_address) {
+
+                let address = await frappe.db.get_doc(
+                    "Address",
+                    supplier.supplier_primary_address
+                );
+
+                frm.set_value(
+                    "custom_country",
+                    address.country || ""
+                );
+
+                frm.set_value(
+                    "custom_state",
+                    address.state || ""
+                );
+
+                frm.set_value(
+                    "custom_city",
+                    address.city || ""
+                );
+            }
+
+        } catch (e) {
+            console.error(e);
+        }
+    }
+});
--- sukha/public/js/item.js
+++ sukha/public/js/item.js
@@ -1,9 +1,37 @@
 frappe.ui.form.on("Item", {
+    setup(frm) {
+
+        frm.fields_dict.custom_standard_packing.grid
+            .get_field("packing_type")
+            .get_query = function (doc, cdt, cdn) {
+
+                let selected_packing_types = (frm.doc.custom_packing_type || [])
+                    .map(row => row.packing_type)
+                    .filter(Boolean);
+
+                console.log("Selected Packing Types:", selected_packing_types);
+
+                return {
+                    filters: {
+                        name: ["in", selected_packing_types]
+                    }
+                };
+            };
+
+        frm.set_query("custom_product_subcategory", function () {
+            return {
+                filters: {
+                    item_group: frm.doc.item_group
+                }
+            };
+        });
+    },
+
     refresh(frm) {
 
         frm.fields_dict.custom_item_compliance_and_export.grid
             .get_field("sub_category_name")
-            .get_query = function(doc, cdt, cdn) {
+            .get_query = function (doc, cdt, cdn) {
 
                 let row = locals[cdt][cdn];
 
